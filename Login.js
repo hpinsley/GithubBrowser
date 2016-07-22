e2dd48c13b1e6f1b4aca805aea8003347e083479
Howard Pinsley <hpinsley@gmail.com>
2016-07-21 20:51:28 -0400

    Displays text in an ES5 component

---
+++ Login.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+var Text = require('react-native').Text;
+
+var Login = React.createClass({
+
+    render: function() {
+        return (
+            <Text>aaa                Hello</Text>
+        );
+    }
+});
+
+module.exports = Login;
