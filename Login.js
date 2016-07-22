7e789ccaf0133d576d00523c0d9db1919a5691ab
Howard Pinsley <hpinsley@gmail.com>
2016-07-21 20:59:22 -0400

    Before switching to ES6 syntax

--- Login.js
+++ Login.js
@@ -1,15 +1,24 @@
 'use strict';
 
 var React = require('react');
-var Text = require('react-native').Text;
+var ReactNative = require('react-native');
+
+var Text = ReactNative.Text;
+var View = ReactNative.View;
+var StyleSheet = ReactNative.StyleSheet;
 
 var Login = React.createClass({
 
     render: function() {
         return (
-            <Text>aaa                Hello</Text>
+            <Text style={styles.container}>Hello</Text>
         );
     }
 });
 
+var styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#F5FCFF'
+    }
+});
 module.exports = Login;
